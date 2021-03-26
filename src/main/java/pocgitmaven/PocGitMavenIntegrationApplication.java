99803cfb7e9dda7e1382aa4a92254ed7fea8d9a3
hihijain <jain.himanshu22@gmail.com>
2021-03-26 19:09:36 +0530

    Base code commit

---
+++ src/main/java/pocgitmaven/PocGitMavenIntegrationApplication.java
@@ -0,0 +1,13 @@
+package pocgitmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocGitMavenIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocGitMavenIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pocgitmaven/PocGitMavenIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package pocgitmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocGitMavenIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
